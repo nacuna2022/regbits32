c9973519bde6f2579c3960f7f040869e78f6c2f4
nacuna <amerei@gmail.com>
2023-11-02 17:32:49 +0800

    set minimum window size to compact and turn of resizing

--- main.c
+++ main.c
@@ -35,8 +35,9 @@ static void activate(GtkApplication* app, gpointer user_data)
         GtkWidget *row;
 
         window = gtk_application_window_new (app);
-        gtk_window_set_title (GTK_WINDOW (window), "regbits32");
-        //gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+        gtk_window_set_title(GTK_WINDOW (window), "regbits32");
+        gtk_window_set_default_size(GTK_WINDOW (window), 1, 1);
+        gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
         
         row = build_regbits();
         gtk_window_set_child(GTK_WINDOW(window), row);
